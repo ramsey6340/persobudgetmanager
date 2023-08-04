bbae36f0f3e87103942d49788c513388dd624ef2
ramsey6340 <eniac5project@gmail.com>
2023-08-04 21:02:59 +0000

    presque la fin de la partie Category

--- src/main/java/com/group3/persobudgetmanager/repositories/CategoryRepository.java
+++ src/main/java/com/group3/persobudgetmanager/repositories/CategoryRepository.java
@@ -5,10 +5,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
     List<Category> findAllByTitleContaining(String keyword);
+    List<Category> findAllByUserIdAndTitleContaining(Long userId, String keyword);
+    Optional<Category> findByTitle(String title);
     List<Category> findAllByUserId(Long id);
     List<Category> findAllByDescriptionContaining(String keyword);
+    List<Category> findAllByUserIdAndDescriptionContaining(Long userId, String keyword);
+    Optional<Category> findByIdAndUserId(Long id, Long userId);
 }
