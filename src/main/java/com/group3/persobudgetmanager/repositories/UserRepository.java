dc59be0c69f3682c7cbc855d693e8b5c462a0f6f
Diak.abdel <yannosco@gmail.com>
2023-08-08 10:40:37 +0000

    first

--- src/main/java/com/group3/persobudgetmanager/repositories/UserRepository.java
+++ src/main/java/com/group3/persobudgetmanager/repositories/UserRepository.java
@@ -1,13 +1,10 @@
 package com.group3.persobudgetmanager.repositories;
 
+
 import com.group3.persobudgetmanager.models.User;
+import io.micrometer.observation.ObservationFilter;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.expression.spel.ast.OpAnd;
-import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
