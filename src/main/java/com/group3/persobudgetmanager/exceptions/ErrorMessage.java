542308f6d6b50c33619263e5784699a9f459fce2
ramsey6340 <eniac5project@gmail.com>
2023-08-08 09:13:07 +0000

    mise à jour des exceptions

--- src/main/java/com/group3/persobudgetmanager/exceptions/ErrorMessage.java
+++ src/main/java/com/group3/persobudgetmanager/exceptions/ErrorMessage.java
@@ -4,4 +4,5 @@ public class ErrorMessage {
     public static String alreadyExist = "La ressource existe déjà";
     public static String notFound = "Une des ressources demandé est introuvable";
     public static String noContent = "Le contenu de la requête est vide";
+    public static String badRequest = "La requete envoyé est incorrecte ou le type de donnée attendue est incorrecte";
 }
