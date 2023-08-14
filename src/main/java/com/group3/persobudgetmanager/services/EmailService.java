6014a0289ca8f8e70473d642e74a477583434cf9
ramsey6340 <98720891+ramsey6340@users.noreply.github.com>
2023-08-14 09:23:59 +0000

    Update EmailServiceImpl.java

--- src/main/java/com/group3/persobudgetmanager/services/EmailService.java
+++ src/main/java/com/group3/persobudgetmanager/services/EmailService.java
@@ -19,7 +19,7 @@ import org.springframework.stereotype.Service;
 @Service
 // Class
 // Implementing EmailService interface
-public class EmailService implements EmailRepository {
+public class EmailRepositoryImpl implements EmailRepository {
 
     @Autowired
     private JavaMailSender javaMailSender;
