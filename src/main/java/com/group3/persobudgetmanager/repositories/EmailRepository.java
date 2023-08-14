100c493602cde574d14238031525c58abc4d7c17
ramsey6340 <98720891+ramsey6340@users.noreply.github.com>
2023-08-14 09:26:46 +0000

    Update and rename EmailService.java to EmailRepository.java

--- src/main/java/com/group3/persobudgetmanager/repositories/EmailRepository.java
+++ src/main/java/com/group3/persobudgetmanager/repositories/EmailRepository.java
@@ -1,6 +1,6 @@
 package com.group3.persobudgetmanager.repositories;
 import com.group3.persobudgetmanager.models.EmailDetails;
-public interface EmailService {
+public interface EmailRepository {
     String sendSimpleMail(EmailDetails details);
     String sendMailWithAttachment(EmailDetails details);
 }
